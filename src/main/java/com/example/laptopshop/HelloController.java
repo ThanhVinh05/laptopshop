705da2bf0b92749e4929315b8c8623afc52b4bf9
Phan Thanh Vinh <vinhptps38840@gmail.com>
2024-12-20 07:50:16 +0700

    helloword

---
+++ src/main/java/com/example/laptopshop/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.laptopshop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Vinh Hello World from Spring Boot!";
+    }
+}
